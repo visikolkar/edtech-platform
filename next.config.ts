9092f003041ae975a9e176bed8a5ae87ee685296
Vishwa Kolkar <visikolkar@gmail.com>
2025-05-01 15:15:34 +0530

    fix: environment variables for deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_HASURA_URL: process.env.NEXT_PUBLIC_HASURA_URL,
+    NEXT_PUBLIC_HASURA_SECRET: process.env.NEXT_PUBLIC_HASURA_SECRET,
+  },
 };
 
 export default nextConfig;
